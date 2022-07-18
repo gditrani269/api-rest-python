0181c457106ce43e95a4eff64a45aeafcff62ba4
german <ubuntu.gdd@gmail.com>
2022-07-18 15:01:46 -0300

    detect tec

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = 'OpenShift Api client',
+    version='1.0.0',
+    description='Uses official Python client library for kubernetes',
+    url='https://github.com/gditrani---/api-rest-python',
+    author='xx',
+    author_email='a@a.com.ar',
+    keywords='openshift api client',
+    include_package_data=True,
+    packages=find_packages()
+)
