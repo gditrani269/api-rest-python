938fe3f63b89f4b005af8c6f2c7dac8cbaa262aa
german <ubuntu.gdd@gmail.com>
2022-07-19 12:28:10 -0300

    main fix

--- api-test.py
+++ api-test.py
@@ -12,16 +12,7 @@ cursor1=conexion1.cursor()
 app = Flask(__name__)
 #cors=CORS(app)
 ### swagger specific ###
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Devops - Apisdesk"
-    }
-)
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
 
 @app.route('/', methods=['GET'])     
 def home(): 
